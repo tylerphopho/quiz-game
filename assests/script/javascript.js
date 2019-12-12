5fc6fb238f79ae32995bc69569fa3dacfede70da
tylerphopho <tyler.phopho@gmail.com>
2019-12-11 17:49:33 -0700

    making adjustments

--- assests/script/javascript.js
+++ assests/script/javascript.js
@@ -1,7 +1,69 @@
-var start = "startBtn"
 
 $(document).ready(function(){
-    $(".start").click(function(){
+    $("#startBtn").on("click", function(){
         $("#startMenu").hide();
     })
-});
+})
+
+
+// Tyler's Code //
+var startBtn = $("#start-btn");
+var  questionsboxEl = $("#questions-box");
+var questionEl = $("#questions");
+var answerBtn = $("#answer-buttons");
+var nextBtn = $("next-btn");
+var questionIndex = 0;
+var score = 0;
+var card = $(".card");
+var seconds = $("#countdown");
+var currentQuestion = questionsArray[questionIndex];
+
+var countdown = setInterval(function(){
+    timer--;
+    document.getElementById("timer").textContent = timer;
+    if (timer <= 0) clearInterval(countdown);
+}, 1000);
+
+// $(document).ready(function(){
+// startBtn.click(startQuiz);
+// answerBtn.click(selectAnswer);
+// nextBtn.click(setNextQuestion);
+
+// function startQuiz() {
+    
+//     var seconds = document.getElementById("countdown").textContent;
+//     var countdown = setInterval(function(){
+//         seconds--;
+//         document.getElementById("countdown").textContent = seconds;
+//         if (seconds <= 0) clearInterval(countdown);
+//     }, 1000);
+    
+//     startBtn.addClass("hide");
+//     questionIndex = 0;
+//     currentQuestion = questionsArray[questionIndex];
+//     questionsboxEl.removeClass("hide");
+//     setNextQuestion(currentQuestion);
+// }
+
+// function showQuestion() {
+//     answerBtn.empty();
+
+//     questionEl.text(currentQuestion.title);
+//     $.each(currentQuestion.choices, function(index, choice){
+//         var newBtn = $("<button>");
+//         newBtn.text(choice);
+//         newBtn.addClass("btn btn-info");
+//         answerBtn.append(newBtn);
+//     })
+
+//     nextBtn.removeClass("hide")
+// }
+
+// function setNextQuestion() {
+//     questionIndex++
+//     showQuestion(currentQuestion);
+
+//     currentQuestion
+// }
+
+// })
